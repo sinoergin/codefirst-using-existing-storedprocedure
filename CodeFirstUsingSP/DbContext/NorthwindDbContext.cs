ddbc542abdd37c2594cb61dec7fe4efd8e5cb69c
Sinan <Sinan@SINANERGIN>
2016-12-05 01:40:04 +0300

    Comment satırları eklenmiştir

--- CodeFirstUsingSP/DbContext/NorthwindDbContext.cs
+++ CodeFirstUsingSP/DbContext/NorthwindDbContext.cs
@@ -14,13 +14,26 @@ namespace CodeFirstUsingSP.DbContext
         {
         }
 
+
+        // StoredProcedure kullanabilmemiz için DbContext tarafýnda dönüþ tipleri ile birlikte method þeklinde yaratýyoruz ve ister Database.SqlQuery istersek ise CRUD methodlarý için kullanabileceðimiz Database.ExecuteSqlCommand methodlarý ile SP'leri çaðýrýyoruz.
+
+        /// <summary>
+        /// ResultGonderimiGerceklesmemisSiparisler methodu GonderimiYapilmamisSiparisBilgileri SP execute edip geriye GenericList döndürecektir.
+        /// </summary>
+        /// <returns></returns>
         public List<GonderimiGerceklesmemisSiparisler> ResultGonderimiGerceklesmemisSiparisler()
         {
             var list =  this.Database.SqlQuery<GonderimiGerceklesmemisSiparisler>(" GonderimiYapilmamisSiparisBilgileri").ToList();
-
+            
             return list;
         }
 
+        
+        /// <summary>
+        /// ResultCalisanaGoreSatisBilgisi methodu CalisanaGoreYapmisOlduguToplamSatisAdediveTutari SP execute edip scalar veri döndürecektir.
+        /// </summary>
+        /// <param name="employeeId"></param>
+        /// <returns></returns>
         public CalisanaGoreSatisBilgisi ResultCalisanaGoreSatisBilgisi(int employeeId)
         {
             if (employeeId == 0)
--- CodeFirstUsingSP/Program.cs
+++ CodeFirstUsingSP/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Linq;
 using CodeFirstUsingSP.DbContext;
-using static System.Console;
+using static System.Console; // Burada C# 6.0 ile gelen using static deyimini kullandık.
 
 namespace CodeFirstUsingSP
 {
@@ -10,14 +10,20 @@ namespace CodeFirstUsingSP
         {
             var dbContext = new NorthwindDbContext();
 
+            // Yapmamız gereken DbContext'ten kullanmak istediğimiz SP'nin methodunu call etmek yeterli olacaktır.
             var calisanSatis = dbContext.ResultCalisanaGoreSatisBilgisi(1);
+
+            // C# 6.0 ile gelen using static ile string interpolation özellikleri ile yazdırma işlemini gerçekleştirdik.
             WriteLine($"Calisan: {calisanSatis.CalisanAdSoyad} SatisTutar: {calisanSatis.SatisTutari}");
 
+            // Aynı şekilde yine using static kullanmış olduk.
             ReadKey();
 
+            // Diğer method da olduğu gibi aynı şekilde burada method call edip set etmemiz yeterli oluyor.
             var siparisBilgisi = dbContext.ResultGonderimiGerceklesmemisSiparisler();
             foreach (var item in siparisBilgisi.Take(10))
             {
+                // Ve benim C# 6.0 ile gelen en beğendiğim özellik: string interpolation :)
                 WriteLine(
                     $"SiparisTarihi: {item.SiparisTarihi.ToString("dd-MM-yyyy")} SiparisAdet:{item.SiparisAdedi} SiparisTutari: {item.SiparisTutari}");
             }
